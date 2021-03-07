4512c4c58ece261f6d4dbae7bdd25a4ca33f2ba9
RogelioOliverC <71861634+RogelioOliverC@users.noreply.github.com>
2021-03-07 16:32:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ setInterval(() => {
   imgElem.src = `images/${randomChoice}.jpg`;
 }, 2000);
 
+}
+
+setInterval(() => {
+  const randomChoice = randomValueFromArray(images);
+  imgElem.src = `images/${randomChoice}.png`;
+}, 2000);
+
 // Register service worker to control making site work offline
 
 if ('serviceWorker' in navigator) {
